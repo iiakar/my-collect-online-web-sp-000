03a79295bae8b389ad306494d9dbdd1a98426f79
Ibrahim  Akar <ibsakar@gmail.com>
2019-10-08 11:52:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,8 @@ def my_collect(languages)
 languages.each do |language| 
   a = language.upcase
   return a 
-  b = a.split(" ").first
-  return b 
+  # b = a.split(" ").first
+  # return b 
  
 
 end 
