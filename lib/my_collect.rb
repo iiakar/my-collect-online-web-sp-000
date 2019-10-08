da82c35fedd7dad1a7fccc2453d828e95ec8c505
Ibrahim  Akar <ibsakar@gmail.com>
2019-10-08 12:00:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,11 @@
 def my_collect(languages)
   
 empty_array = []
+new_array = []
    counter = 0
   languages.each do |x|
     empty_array << x.upcase
-    empty_array << x.split(" ").first
+    new_array << x.split(" ").first
     counter += 1
     end
     empty_array
