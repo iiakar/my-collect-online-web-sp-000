4b96aaf3e931df31b3a534d66837d06dbdcfc7e4
Ibrahim  Akar <ibsakar@gmail.com>
2019-10-08 12:04:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,3 @@
 def my_collect(languages)
-  
-capitalized_array = []
-new_array = []
-   counter = 0
-  languages.each do |x|
-    capitalized_array << x.upcase
-    new_array << x.split(" ").first
-    counter += 1
-    end
-    capitalized_array
-    new_array
-  end 
+  i = 0
+counter
