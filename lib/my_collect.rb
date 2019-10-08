dd2aedf48b08c982d21608fa8633c36b6bd1600d
Ibrahim  Akar <ibsakar@gmail.com>
2019-10-08 11:50:48 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,10 @@
 def my_collect(languages)
-  
+  collection =[]
 languages.each do |language| 
   puts language
-
-
+ collection << yield(languages)
+ collection.each do |name|
+   puts name.upcase
+end 
 end 
 end
